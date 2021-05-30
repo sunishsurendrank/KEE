1dad476eb1de2403458d19047243d95900691d10
Sunish <sunish.k@nokia.com>
2021-05-30 17:30:28 +0530

    Adding docker file

--- event_manager/main.go
+++ event_manager/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"github.com/gocql/gocql"
 	"time"
+   "os"
 )
 
 
@@ -31,57 +32,69 @@ func main(){
 	}
 
 
-   cluster := gocql.NewCluster("10.157.50.15:9042") //replace PublicIP with the IP addresses used by your cluster.
+   cassandra_server:= os.Getenv("CASSANDRA_SERVER")
+   event_query_service:= os.Getenv("EVENT_QUERY_SERVICE")
+   // Connecting to the Database
+
+   cluster := gocql.NewCluster(cassandra_server)
    cluster.Consistency = gocql.Quorum
    cluster.ProtoVersion = 4
    cluster.ConnectTimeout = time.Second * 10
-   //cluster.Authenticator = gocql.PasswordAuthenticator{Username: "Username", Password: "Password"} //replace the username and password fields with their real settings.
    session, err := cluster.CreateSession()
-
    if err != nil {
-	fmt.Println(err)
-	return
+      fmt.Println(err)
+   return
    }
    defer session.Close()
    
-   // create keyspaces
-   err = session.Query("CREATE KEYSPACE IF NOT EXISTS events_keyspace WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};").Exec()
 
-   if err != nil {
-   fmt.Println(err)
-   return
+   for {
+
+
+               
+               // create keyspaces
+               err = session.Query("CREATE KEYSPACE IF NOT EXISTS events_keyspace WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};").Exec()
+               if err != nil {
+                  fmt.Println(err)
+               return
+               }
+               
+               // create table
+               err = session.Query("CREATE TABLE IF NOT EXISTS events_keyspace.events_table (eventname text, event_creationTimestamp date, kind text,namespace text,name text,message text,reason text,firstTimestamp date,lastTimestamp date,type text,  PRIMARY KEY (event_creationTimestamp, eventname));").Exec()
+               if err != nil {
+                  fmt.Println(err)
+               return
+               }
+
+            // Fetching the  event query microservice
+
+               resp, err := http.Get(event_query_service)
+
+               if err != nil {
+                  fmt.Println(err)
+               }
+
+               body, err := ioutil.ReadAll(resp.Body)
+
+               if err != nil {
+                  fmt.Println(err)
+               }
+
+               var events [] Event
+               json.Unmarshal(body,&events)
+
+               for _, event := range events {
+
+                  DBquery := fmt.Sprintf("INSERT INTO events_keyspace.events_table (eventname,event_creationTimestamp,kind,namespace,name,message,reason,firstTimestamp,lastTimestamp,type) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');", event.Eventname, event.Event_creationTimestamp, event.Kind,event.Namespace, event.Name, event.Message, event.Reason, event.FirstTimestamp, event.LastTimestamp, event.Typename)
+                     fmt.Println(DBquery)
+                     err = session.Query(DBquery).Exec()
+                     if err != nil {
+                        fmt.Println(err)
+                        }
+                     fmt.Println("******************************************")
+                     
+               }
+
    }
-   
-   // create table
-   err = session.Query("CREATE TABLE IF NOT EXISTS events_keyspace.events_table (eventname text, event_creationTimestamp date, kind text,namespace text,name text,message text,reason text,firstTimestamp date,lastTimestamp date,type text,  PRIMARY KEY (event_creationTimestamp, eventname));").Exec()
-   if err != nil {
-   fmt.Println(err)
-   return
-   }
-
-  
-   resp, err := http.Get("http://127.0.0.1:5000/events")
-   if err != nil {
-	fmt.Println(err)
-   }
-   body, err := ioutil.ReadAll(resp.Body)
-   if err != nil {
-	fmt.Println(err)
-   }
-   var events [] Event
-   json.Unmarshal(body,&events)
-
-   for _, event := range events {
-
-
-	DBquery := fmt.Sprintf("INSERT INTO events_keyspace.events_table (eventname,event_creationTimestamp,kind,namespace,name,message,reason,firstTimestamp,lastTimestamp,type) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');", event.Eventname, event.Event_creationTimestamp, event.Kind,event.Namespace, event.Name, event.Message, event.Reason, event.FirstTimestamp, event.LastTimestamp, event.Typename)
-	fmt.Println(DBquery)
-	err = session.Query(DBquery).Exec()
-	if err != nil {
-		fmt.Println(err)
-	   }
-    fmt.Println("******************************************")
-   
-}
 
 }
